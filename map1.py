49ef9bf8cae195a256e9a44191fe479f0fc86110
Evan Hu <huhaoxiangwow@gmail.com>
2022-01-05 15:48:46 +0800

    Adding a Layer COntrol Panel

--- map1.py
+++ map1.py
@@ -1,4 +1,5 @@
 import folium
+from folium.map import LayerControl
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
@@ -20,18 +21,25 @@ Height: %s m
 
 map = folium.Map(location = [37.43, -121.89], zoom_start=6, tiles = "Stamen Terrain")
 
-fg = folium.FeatureGroup(name = "My Map")
+fgv = folium.FeatureGroup(name = "Volcanoes")
+
 for lt, ln, el in zip(lat, lon, elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location = [lt,ln], radius = 6, popup=folium.Popup(iframe), 
+    fgv.add_child(folium.CircleMarker(location = [lt,ln], radius = 6, popup=folium.Popup(iframe), 
     fill_color = color_producer(el), color = 'black', fill = True, fill_opacity = 0.7))
 
+
 #Stylizing the Population Layer
-fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name = "Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000 
 else 'orange' if 1000000 <= x['properties']['POP2005'] < 2000000
 else 'red'}))
 
-map.add_child(fg)
+#Adding a Layer Control Panel
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
